7596c04b9bc7a77e2c62952f6875fd1847759842
Alex Yu <alex@NEBULA.milkyway.net>
2019-11-24 14:10:17 -0500

    kendrick

--- src/factors.js
+++ src/factors.js
@@ -1,8 +1,12 @@
 const readlineSync = require("readline-sync");
 
-let positiveInteger = readlineSync.question("Positive integer: ");
+let positiveInteger = NaN;
 
 console.log();
 while (positiveInteger > Number.MAX_SAFE_INTEGER || positiveInteger < 1 || Number.isNaN(positiveInteger) == true || positiveInteger%1!=0){
   positiveInteger = Number(readlineSync.question("Positive integer: "));
+}
+
+for (i = 1; i < (positiveInteger / 2); i++) {
+  
 }
