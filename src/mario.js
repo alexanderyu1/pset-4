60e2e133c49c7a99a19b361d54f23d96408f5e07
Alex Yu <alex@NEBULA.milkyway.net>
2019-11-25 23:01:18 -0500

    i remember u was conflicted misusing your influence

--- src/mario.js
+++ src/mario.js
@@ -8,4 +8,14 @@ while (height > 24 || height < 1 || Number.isNaN(height) == true || height % 1 !
   height = Number(readlineSync.question("Height: "));
 }
 
-let layer = height;
+console.log();
+
+for(x = 1; x < height + 1; x++){
+  for(var i = 0; i < height - x; i++){
+    pyramid = pyramid + " ";
+  }for(var i = 0; i < x + 1; i++){
+    pyramid = pyramid + "#";
+  }
+  console.log(pyramid);
+  pyramid = "";
+}
