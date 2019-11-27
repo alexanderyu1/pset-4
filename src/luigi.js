c6d43ed31f59f8d35d2573b7eb64c38bbf6485ea
Alex Yu <alex@NEBULA.milkyway.net>
2019-11-26 21:13:33 -0500

    wesleys theory

--- src/luigi.js
+++ src/luigi.js
@@ -1,8 +1,34 @@
 const readlineSync = require("readline-sync");
 
 let height = NaN;
+let pyramidContent;
+let pyramidContentMirror;
 
 console.log();
 while (height > 24 || height < 1 || Number.isNaN(height) == true || height % 1 != 0) {
   height = Number(readlineSync.question("Height: "));
 }
+
+console.log();
+
+let layer = height;
+
+while (layer > 0) {
+    pyramidContent = "";
+    pyramidContentMirror = "";
+
+    for (let i = 0; i <= height; i++) {
+        pyramidContent = pyramidContent + "#";
+    }
+    for (let i = 0; i < layer - 1; i++) {
+        pyramidContent = pyramidContent.replace(pyramidContent.charAt(i), " ");
+    }
+    for (let i = layer - 2; i < height; i++) {
+        pyramidContentMirror = pyramidContentMirror + "#";
+    }
+
+    console.log(pyramidContent + " " + pyramidContentMirror);
+    layer--;
+}
+
+console.log();
